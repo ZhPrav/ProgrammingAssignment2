330934087d58d6bfad895c78f28b89f224fbb4b7
ZhPrav <j_prav@yahoo.com>
2018-01-22 15:33:20 -0500

    cachematrix.R updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 cource R Programming
+## functions: 1. Creates a special matrix object with methods
+##            2. Calculates the matrix inverse or get the cached matrix inverse
 
-## Write a short comment describing this function
+## Function makeCacheMatrix() creates a special matrix object with methods
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_m <- NULL
+  init_x <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_m <<- NULL
+    print("creating a new matrix with set")
+  }
+  get <- function() x
+  setSolve <- function(solve) inv_m <<- solve
+  getSolve <- function() inv_m
+  list(set = set, 
+       get = get,
+       setSolve = setSolve,
+       getSolve = getSolve 
+       )
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Function cacheSolve() calculates the matrix inverse or get the cached matrix inverse
+cacheSolve <- function(o, ...) {
+  inv_m <- o$getSolve() 
+  init_x <- o$get()
+  ##print("printing init_x")
+  ##print(init_x)
+  if(!is.null(inv_m) )
+    {
+    message("getting cached data")
+    return(inv_m)
+  }
+  data <- o$get()
+  inv_m <- solve(data, ...)
+  o$setSolve(inv_m)
+  inv_m
 }
